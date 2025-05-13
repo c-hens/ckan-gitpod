3b5f198da78b24f9436fd8fb43bcbd6af7600017
c-hens <chens@tragsa.es>
2025-05-13 13:52:50 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "✅ Entorno listo. Ahora puedes instalar CKAN desde aquí."
+
+python3 --version
+pip3 --version
+
+echo "Instalación básica completa. CKAN aún no instalado."
